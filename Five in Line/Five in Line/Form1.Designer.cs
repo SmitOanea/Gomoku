240fe23ed38be87b6012da3c7656beac4eec8956
Șmit-Andrei Oanea <36793993+SmitOanea@users.noreply.github.com>
2019-04-11 21:28:52 +0300

    Add files via upload

---
+++ Five in Line/Five in Line/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Five_in_Line
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Location = new System.Drawing.Point(15, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 0);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 473);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Turn:";
+            this.label1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Five_in_Line.Properties.Resources.rosu;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 468);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(18, 18);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bine ati veni!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Introduceti marimea tablei de joc:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(246, 176);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(289, 176);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 687);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Five in Line";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ Five in Line/Five in Line/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Five_in_Line
+{
+
+    public partial class Form1 : Form
+    {
+        int turn = 0, h;
+        int nr_patratele_ocupate = 0;
+        bool game_ended = false;
+        Button[][] buttonArray = new Button[20][];
+        Button buff_red = new Button(), buff_blue = new Button();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GenereazaTabla()
+        {
+            for (int i = 0; i <= h - 1; i++)
+            {
+                buttonArray[i] = new Button[h];
+                for (int j=0;j<h;++j)
+                {
+                    buttonArray[i][j] = new Button();
+                    buttonArray[i][j].Size = new Size(25, 25 );
+                    buttonArray[i][j].Name = "" + i*h+j + "";
+                    buttonArray[i][j].Click += button_Click;//function
+                    buttonArray[i][j].Location = new Point(20*j, 20 + (i * 20));
+                    panel1.Controls.Add(buttonArray[i][j]);
+                }
+            }
+            panel1.Location = new Point(
+            this.ClientSize.Width / 2 - panel1.Size.Width / 2,
+            this.ClientSize.Height *3/10 - panel1.Size.Height / 2);
+            panel1.Anchor = AnchorStyles.None;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buff_blue.BackgroundImage = Properties.Resources.albastru;
+            buff_blue.BackgroundImageLayout = ImageLayout.Stretch;
+            buff_red.BackgroundImage = Properties.Resources.rosu;
+        }
+
+        bool Victorie()
+        {
+            ///Verific daca a castigat cineva
+            int i, j, secv;
+            secv = 1;
+            //orizontala
+            for (i = 0; i < h; ++i)
+            {
+                for (j = 0; j < h; ++j)
+                {
+                    if (j > 0 && buttonArray[i][j].BackgroundImage != null && buttonArray[i][j].BackgroundImage == buttonArray[i][j - 1].BackgroundImage)
+                        secv++;
+                    else
+                        secv = 1;
+                    if (secv == 5)
+                    {
+                        for (int jj = j; jj >= j - 5 + 1; --jj)
+                        {
+                            buttonArray[i][jj].BackColor = Color.Green;
+                        }
+                        return true;
+                    }
+                }
+            }
+            //verticala
+            for(j = 0; j < h; ++j)
+            {
+                for (i = 0; i < h; ++i)
+                {
+                    if (i > 0 && buttonArray[i][j].BackgroundImage != null && buttonArray[i][j].BackgroundImage == buttonArray[i-1][j].BackgroundImage)
+                        secv++;
+                    else
+                        secv = 1;
+                    if (secv == 5)
+                    {
+                        for (int ii = i; ii >= i - 5 + 1; --ii)
+                        {
+                            buttonArray[ii][j].BackColor = Color.Green;
+                        }
+                        return true;
+                    }
+                }
+            }
+
+
+            int x, y;
+            //diagonale paralele cu diagonala principala
+            for(i=0;i<h;i++)
+                for(j=0;j<h;++j)
+                {
+                    x = i;
+                    y = j;
+                    secv = 1;
+                    while(x<h && y<h)
+                    {
+                        if (x > 0 && y > 0 && buttonArray[x][y].BackgroundImage != null && buttonArray[x][y].BackgroundImage == buttonArray[x - 1][y - 1].BackgroundImage)
+                            secv++;
+                        else
+                            secv = 1;
+                        if(secv == 5)
+                        {
+                            for(int val = 0; val < 5; val++)
+                                buttonArray[x - val][y - val].BackColor = Color.Green;
+                            return true;
+                        }
+                        x++;
+                        y++;
+
+                    }
+                }
+
+            //diagonale paralele cu diagonala secundara
+            for (i = 0; i < h; i++)
+                for (j = 0; j < h; ++j)
+                {
+                    x = i;
+                    y = j;
+                    secv = 1;
+                    while (x >= 0 && y < h)
+                    {
+                        if (x < h-1 && y > 0 && buttonArray[x][y].BackgroundImage != null && buttonArray[x][y].BackgroundImage == buttonArray[x + 1][y - 1].BackgroundImage)
+                            secv++;
+                        else
+                            secv = 1;
+                        if (secv == 5)
+                        {
+                            for (int val = 0; val < 5; val++)
+                                buttonArray[x + val][y - val].BackColor = Color.Green;
+                            return true;
+                        }
+                        x--;
+                        y++;
+
+                    }
+                }
+
+
+            return false;
+
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            
+            if (b.BackgroundImage != null)
+                return;//sa nu pot desena peste un patratel deja folosit
+            if (game_ended)
+                return;
+            
+            if (turn==0)//rosu
+            {
+                b.BackgroundImage = buff_red.BackgroundImage;
+                b.BackgroundImageLayout = ImageLayout.Stretch;           
+            }
+            else
+                if(turn==1)//albastru
+                {
+                    b.BackgroundImage = buff_blue.BackgroundImage;
+                    b.BackgroundImageLayout = ImageLayout.Stretch;
+                }
+
+            nr_patratele_ocupate++;
+
+            //MessageBox.Show("nr_patratele_ocupate = " + nr_patratele_ocupate + "\n" + "h*h = " + h * h); 
+
+            if (Victorie())
+            {
+                game_ended = true;
+                MessageBox.Show("Castigatorul este: " + turn);
+            }
+
+            if (nr_patratele_ocupate == h * h)//s-a facut ultima mutare posibila
+            {                                 //nu mai sunt patratele libere
+                if(!game_ended)
+                    MessageBox.Show("Remiza!");
+            }
+
+            if (turn == 0)
+            {
+                turn = 1;
+                pictureBox1.BackgroundImage = Properties.Resources.albastru;
+               
+            }
+            else
+            {
+                if (turn == 1)
+                {
+                    turn = 0;
+                    pictureBox1.BackgroundImage = Properties.Resources.rosu;
+                }
+                else
+                    MessageBox.Show("Eroare in button_Click");
+            }
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            h = (int)numericUpDown1.Value;
+            label1.Visible = true;
+            pictureBox1.Visible = true;
+            label2.Visible = false;
+            label3.Visible = false;
+            numericUpDown1.Visible = false;
+            buttonStart.Visible = false;
+            GenereazaTabla();
+        }
+    }
+}
